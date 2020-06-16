51102c606e43e93b65d1dc2374cd904ed25835f6
John K. Speck <speckworks@gmail.com>
2020-06-16 19:18:06 +0000

    optimized solution

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 // Complete the minimumBribes function below.
 function minimumBribes(q) {
- console.log(sort(q));
-    function sort(items) {
-     let bribes = 0;
-
-     for (let i = 0; i < items.length; i++) {
-         if (items[i] - (i + 1) > 2) return "Too chaotic";
-         for (let j = 0; j < i; j++) {
-             if (items[j] > items[i]) bribes++;
-         }
-     } 
-     return bribes;
-   }
+   let bribes = 0, i, j;
+    for (i = 0; i < q.length; i++) {
+        const pos = q[i], at = i + 1;
+        if (pos - at > 2) { return "Too chaotic" } 
+        for (j = Math.max(0, pos - 2); j < i; j++) {
+            if (q[j] > pos) { bribes++ }
+        }
+    } 
+return bribes;
 }
